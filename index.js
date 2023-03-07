19c2b5f2d2b60e6cb65db24da0a06cdf4f8159e5
Umang Jaiswal <jaiswalumang01@gmail.com>
2023-03-07 20:17:45 +0530

    setup static files access for layouts and pages

---
+++ index.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const app = express();
+const port= 8000;
+const expressLayouts = require('express-ejs-layouts');
+const db = require('./config/mongoose');
+
+
+app.use(express.static('./assets'));
+
+//use express layouts
+app.use(expressLayouts);
+
+// use express router
+app.use('/',require('./routes'));
+
+//set up the view engine
+app.set('view engine','ejs');
+app.set('views', './views');
+
+app.listen(port , function (err){
+    if (err){
+        console.log (`Error in running the server: ${err}`); 
+    }
+    console.log(`This server is running in the port: ${port}`);
+});
