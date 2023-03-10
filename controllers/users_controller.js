50070d5942f4228d84c8745750198a1c8a0addd2
Umang Jaiswal <jaiswalumang01@gmail.com>
2023-03-10 15:47:45 +0530

    set the log in and logout for the users with password encrypted

--- controllers/users_controller.js
+++ controllers/users_controller.js
@@ -1,5 +1,108 @@
-module.exports.profile = function(req,res){
-   return res.render('user_profile',{
-    title: 'User Profile'
-   })
+const User = require('../models/user');
+
+
+module.exports.profile = async function(req, res){
+   try {
+       const userId = req.cookies.user_id;
+       if (userId) {
+           const user = await User.findById(userId);
+           if (user) {
+               return res.render('user_profile', {
+                   title: 'User Profile',
+                   user: user
+               });
+           } else {
+               return res.redirect('/users/sign-in');
+           }
+       } else {
+           return res.redirect('/users/sign-in');
+       }
+   } catch (err) {
+       console.log('Error in rendering user profile page:', err);
+       return;
+   }
+}
+
+// render the sign up page
+module.exports.signUp = async function(req, res) {
+   try {
+     res.render('user_sign_up', {
+       title: 'SocialBuzz | Sign Up'
+     });
+   } catch (err) {
+     console.log('Error in rendering sign-up page:', err);
+     return;
+   }
+ };
+ 
+// render the sign in page
+module.exports.signIn = async function(req, res) {
+   try {
+     res.render('user_sign_in', {
+       title: 'SocialBuzz | Sign In'
+     });
+   } catch (err) {
+     console.log('Error in rendering sign-in page:', err);
+     return;
+   }
+ };
+ 
+
+module.exports.create = async function(req, res) {
+   try {
+     if (req.body.password !== req.body.confirm_password) {
+       return res.redirect('back');
+     }
+ 
+     const user = await User.findOne({ email: req.body.email }).exec();
+     if (user) {
+       return res.redirect('back');
+     }
+ 
+     const newUser = await User.create(req.body);
+     return res.redirect('/users/sign-in');
+   } catch (err) {
+     console.log('Error in finding or creating user in signup', err);
+     return;
+   }
+ };
+ 
+// sign in and create a session
+module.exports.createSession = async function (req, res) {
+   try {
+     // find the user 
+     const user = await User.findOne({ email: req.body.email });
+ 
+     // handle user not found 
+     if (!user) {
+       return res.redirect('back');
+     }
+ 
+     // handle password which dont match
+     if (user.password !== req.body.password) {
+       return res.redirect('back');
+     }
+ 
+     // handle session creation
+     res.cookie('user_id', user.id);
+     return res.redirect('/users/profile');
+   } catch (err) {
+     console.log('Error in finding user in signing in');
+     return;
+   }
+};
+
+//  module.exports.createSession = function (req,res){
+//     return res.redirect('/');
+//  }
+
+
+// for sign out
+module.exports.destroySession = function(req,res){
+ req.logout(function(err){
+  if(err){
+    return next(err);
+  }
+  return res.redirect('/');
+ })
 }
--- index.js
+++ index.js
@@ -1,25 +1,68 @@
 const express = require('express');
+const cookieParser = require('cookie-parser');
+const bodyParser = require('body-parser');
+
 const app = express();
 const port= 8000;
 const expressLayouts = require('express-ejs-layouts');
 const db = require('./config/mongoose');
 
+//used for session cookie
+const session = require('express-session');
+const passport = require('passport');
+const passportLocal = require('./config/passport-local-strategy');
+const  MongoStore =  require('connect-mongo');
+
+
+//app.use(express.urlencoded());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.use(cookieParser());
 
 app.use(express.static('./assets'));
 
 //use express layouts
 app.use(expressLayouts);
 
-// use express router
-app.use('/',require('./routes'));
+
 
 //set up the view engine
 app.set('view engine','ejs');
 app.set('views', './views');
 
-app.listen(port , function (err){
+// mongo store is used to store the session cookie  in the db
+app.use(session({
+
+    name:'SocialApp',//cookie name
+    secret:'jkajksj',
+    saveUninitialized:false,
+    resave:false,
+    cookie:
+    {
+        maxAge:(1000*60*100)
+    },
+    store: MongoStore.create({ 
+        mongoUrl: 'mongodb://127.0.0.1:27017'},
+    function(err){
+        console.log(err);
+    })
+}));
+  
+
+app.use (passport.initialize());
+app.use (passport.session());
+
+app.use (passport.setAuthenticatedUser);
+// use express router
+app.use('/',require('./routes'));
+
+
+
+
+app.listen(port, async function (err){
     if (err){
-        console.log (`Error in running the server: ${err}`); 
+        console.log(`Error in running the server: ${err}`);
+    } else {
+        console.log(`This server is running in the port: ${port}`);
     }
-    console.log(`This server is running in the port: ${port}`);
 });
