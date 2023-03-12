7f7a90df9c1f176d8232b5dcbfc3f587ca3de81e
Umang Jaiswal <jaiswalumang01@gmail.com>
2023-03-12 14:47:08 +0530

    added a post form

--- controllers/home_controller.js
+++ controllers/home_controller.js
@@ -1,15 +1,28 @@
-// module.exports.home =  function(req, res){
-//     console.log(req.cookies);
-//     //res.cookie('user_id',25);
-//     return res.render('home', {
-//         title: "Home"
-//     });}
-module.exports.home = function(req, res){
-    console.log(req.cookies);
-    res.cookie('user_id', 25);
-    return res.render('home', {
-        title: "Home"
-    });
-}
+const Post =  require('../models/post');
 
-// module.exports.actionName = function(req, res){}
+// module.exports.home = async function(req, res){
+//     try {
+//         const posts = await Post.find({}).exec();
+//         return res.render('home',{
+//             title : "SocialBuzz | Home",
+//             posts : posts
+//         })
+//     } catch (err) {
+//         console.error(err);
+//         return res.status(500).send("Internal Server Error");
+//     }
+// }
+// populate of each post
+module.exports.home = async function(req, res) {
+    try {
+      const posts = await Post.find({}).populate('user').exec();
+      return res.render('home', {
+        title: "SocialBuzz | Home",
+        posts: posts
+      });
+    } catch (err) {
+      console.error(err);
+      return res.status(500).send("Internal Server Error");
+    }
+  };
+  
